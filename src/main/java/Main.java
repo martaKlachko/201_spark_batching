6e9165948002fdb5a6633442c9a6f5079ed607b7
Marta_Kurman <marta.klachko@gmail.com>
2020-06-30 22:07:01 +0300

    added expedia

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,12 +6,15 @@ import org.apache.spark.sql.SparkSession;
 public class Main {
     public static void main(String[] args) {
         SparkSession spark = SparkSession.builder().master("yarn").appName("Application").enableHiveSupport().getOrCreate();
-        Dataset<Row> ds = spark.read().option("header", "false")
+        Dataset<Row> hotels_weather_joined = spark.read().option("header", "false")
                 .csv("hdfs://sandbox-hdp.hortonworks.com:8020/apps/hive/warehouse/hotels_weather_joined");
 
-
-        long count= ds.count();
-                System.out.println("count=" +count);
+        Dataset<Row> expedia = spark.read().option("header", "false")
+                .csv("hdfs://sandbox-hdp.hortonworks.com:8020/apps/hive/warehouse/expedia");
+        long counth= hotels_weather_joined.count();
+        long counte= expedia.count();
+                System.out.println("count hotels_weather_joined=" +counth);
+                System.out.println("count expedia=" +counte);
 
         spark.stop();
     }
