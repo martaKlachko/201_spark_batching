bc39d9f21c0239bb42a3d5b0c41ca7310a6970de
Marta_Kurman <marta.klachko@gmail.com>
2020-06-30 18:02:05 +0300

    added spark session creating

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,17 @@
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello");
+        SparkSession spark = SparkSession.builder().master("yarn").appName("Application").enableHiveSupport().getOrCreate();
+        Dataset<Row> ds = spark.read()
+                .parquet("hdfs://sandbox-hdp.hortonworks.com:8020/apps/hive/warehouse/hotels_weather_joined");
+
+        int count= ds.count()
+                System.out.println("count=" +count);
+
+        spark.stop();
     }
 }
