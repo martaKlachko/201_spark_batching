f31e169691ee6bccfdaf16d064ff0d5c4a198b78
Marta_Kurman <marta.klachko@gmail.com>
2020-06-30 22:40:12 +0300

    refactoring

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,18 +8,22 @@ public class Main {
         SparkSession spark = SparkSession.builder().master("yarn").appName("Application").enableHiveSupport().getOrCreate();
         Dataset<Row> hotels_weather_joined = spark.read().option("header", "false")
                 .csv("hdfs://sandbox-hdp.hortonworks.com:8020/apps/hive/warehouse/hotels_weather_joined");
-
         Dataset<Row> expedia = spark.read().option("header", "false")
                 .csv("hdfs://sandbox-hdp.hortonworks.com:8020/apps/hive/warehouse/expedia");
-        long counth= hotels_weather_joined.count();
-        long counte= expedia.count();
-                System.out.println("count hotels_weather_joined=" +counth);
-                System.out.println("count expedia=" +counte);
-        Dataset<Row> expedia_hotels_weather_joined = hotels_weather_joined
-                .join(expedia, hotels_weather_joined.col("id").equalTo(expedia.col("id")));
+//        long counth= hotels_weather_joined.count();
+//        long counte= expedia.count();
+                System.out.println(" hotels_weather_joined=" );
 
-        long countexp= expedia_hotels_weather_joined.count();
-        System.out.println("count joined=" +countexp);
+
+                hotels_weather_joined.printSchema();
+                System.out.println(" expedia=" );
+                 expedia.printSchema();
+
+//        Dataset<Row> expedia_hotels_weather_joined = hotels_weather_joined
+//                .join(expedia, hotels_weather_joined.col("id").equalTo(expedia.col("id")));
+//
+//        long countexp= expedia_hotels_weather_joined.count();
+//        System.out.println("count joined=" +countexp);
 
 
         spark.stop();
