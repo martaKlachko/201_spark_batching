aecb6a385e2fe8b60a4e5a292b398663243ba85c
Marta_Kurman <marta.klachko@gmail.com>
2020-07-03 14:09:23 +0300

    refactoring

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -30,11 +30,11 @@ public class Main {
               .join(weather_rounded, hotels_rounded.col("Latitude_rounded").equalTo(weather_rounded.col("lat_rounded"))
                       .and(hotels_rounded.col("Longitude_rounded").equalTo(weather_rounded.col("lng_rounded"))));
 
-        Dataset<Row>  expedia_hotels_weather_joined = hotels_rounded
-                .join(expedia, hotels_rounded.col("Id").equalTo(expedia.col("id")));
+//        Dataset<Row>  expedia_hotels_weather_joined = hotels_rounded
+//                .join(expedia, hotels_rounded.col("Id").equalTo(expedia.col("id")));
 
 //      System.out.println("count joined=" );
-        expedia_hotels_weather_joined.show(10);
+        hotels_weather_joined.show(10);
 
         spark.stop();
     }
