7aa7a740c2a4c8334dad9eef91a77c782cdf6993
Marta_Kurman <marta.klachko@gmail.com>
2020-07-06 17:14:24 +0300

    refactoring

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,16 +8,16 @@ public class Main {
 
     public static void main(String[] args) {
         SparkSession spark = SparkSession.builder()
-              //  .config("spark.jars", "/home/maria_dev/201_spark_batching/target/201_project_batching-1.0-SNAPSHOT.jar")
+                //  .config("spark.jars", "/home/maria_dev/201_spark_batching/target/201_project_batching-1.0-SNAPSHOT.jar")
                 .enableHiveSupport().getOrCreate();
         Dataset<Row> hotels = spark.read().option("header", "true")
                 .csv("hdfs://sandbox-hdp.hortonworks.com:8020/201_hotels");
 
-        Dataset<Row>   hotels_rounded= hotels.withColumn("Latitude_rounded", functions.round(hotels.col("Latitude"), 2))
+        Dataset<Row> hotels_rounded = hotels.withColumn("Latitude_rounded", functions.round(hotels.col("Latitude"), 2))
                 .withColumn("Longitude_rounded", functions.round(hotels.col("Longitude"), 2));
         Dataset<Row> weather = spark.read()
                 .parquet("hdfs://sandbox-hdp.hortonworks.com:8020/201_weather/*");
-        Dataset<Row>   weather_rounded= weather.withColumn("lat_rounded", functions.round(weather.col("lat"), 2))
+        Dataset<Row> weather_rounded = weather.withColumn("lat_rounded", functions.round(weather.col("lat"), 2))
                 .withColumn("lng_rounded", functions.round(weather.col("lng"), 2));
         Dataset<Row> expedia = spark.read().format("com.databricks.spark.avro").option("header", "true")
                 .load("hdfs://sandbox-hdp.hortonworks.com:8020/apps/hive/warehouse/expedia");
@@ -28,11 +28,11 @@ public class Main {
 //        System.out.println(" weather=");
 //        weather_rounded.limit(10).show();
 
-       Dataset<Row>  hotels_weather_joined = hotels_rounded
-              .join(weather_rounded, hotels_rounded.col("Latitude_rounded").equalTo(weather_rounded.col("lat_rounded"))
-                      .and(hotels_rounded.col("Longitude_rounded").equalTo(weather_rounded.col("lng_rounded"))));
+        Dataset<Row> hotels_weather_joined = hotels_rounded
+                .join(weather_rounded, hotels_rounded.col("Latitude_rounded").equalTo(weather_rounded.col("lat_rounded"))
+                        .and(hotels_rounded.col("Longitude_rounded").equalTo(weather_rounded.col("lng_rounded"))));
 
-        Dataset<Row>  expedia_hotels_weather_joined = hotels_rounded
+        Dataset<Row> expedia_hotels_weather_joined = hotels_rounded
                 .join(expedia, hotels_rounded.col("Id").equalTo(expedia.col("id")));
 
 //      System.out.println("count joined=" );
