82937864a144d7fc11dba118dd07abd5b2be36f5
Marta_Kurman <marta.klachko@gmail.com>
2020-07-20 23:30:29 +0300

    refactoring

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -15,13 +15,13 @@ public class Main {
 
         Dataset<Row> hotels = spark.read().option("header", "true")
                 .csv("hdfs://sandbox-hdp.hortonworks.com:8020/201_hotels");
-        Dataset<Row> hotels_rounded = hotels.withColumn("Latitude_rounded", functions.round(hotels.col("Latitude"), 2))
-                .withColumn("Longitude_rounded", functions.round(hotels.col("Longitude"), 2));
+//        Dataset<Row> hotels_rounded = hotels.withColumn("Latitude_rounded", functions.round(hotels.col("Latitude"), 2))
+//                .withColumn("Longitude_rounded", functions.round(hotels.col("Longitude"), 2));
 
         Dataset<Row> weather = spark.read()
                 .parquet("hdfs://sandbox-hdp.hortonworks.com:8020/201_weather");
-        Dataset<Row> weather_rounded = weather.withColumn("lat_rounded", functions.round(weather.col("lat"), 2))
-                .withColumn("lng_rounded", functions.round(weather.col("lng"), 2));
+//        Dataset<Row> weather_rounded = weather.withColumn("lat_rounded", functions.round(weather.col("lat"), 2))
+//                .withColumn("lng_rounded", functions.round(weather.col("lng"), 2));
 
         Dataset<Row> expedia = spark.read()
                 .format("com.databricks.spark.avro")
@@ -31,33 +31,40 @@ public class Main {
 //                .join(weather_rounded, hotels_rounded.col("Latitude_rounded").equalTo(weather_rounded.col("lat_rounded"))
 //                .and(hotels_rounded.col("Longitude_rounded").equalTo(weather_rounded.col("lng_rounded"))));
 
-        Dataset<Row> expedia_hotels_joined = hotels_rounded
-                .join(expedia, hotels_rounded.col("Id").equalTo(expedia.col("hotel_id")));
+        Dataset<Row> expedia_hotels_joined = hotels
+                .join(expedia, hotels.col("Id").equalTo(expedia.col("hotel_id")));
 
 
         WindowSpec window = Window.partitionBy("hotel_id").orderBy("hotel_id");
 
+//        Dataset<Row> df = expedia
+//                .withColumn("srch_ci_date", expedia_hotels_joined.col("srch_ci").cast("date"))
+//                .withColumn("srch_co_date", expedia_hotels_joined.col("srch_co").cast("date"))
+//                .orderBy("hotel_id", "srch_ci_date")
+//                .withColumn("lag_day", functions.lag("srch_ci_date", 1)
+//                        .over(window));
+
         Dataset<Row> df = expedia
-                .withColumn("srch_ci_date", expedia_hotels_joined.col("srch_ci").cast("date"))
-                .withColumn("srch_co_date", expedia_hotels_joined.col("srch_co").cast("date"))
-                .orderBy("hotel_id", "srch_ci_date")
-                .withColumn("lag_day", functions.lag("srch_ci_date", 1)
+//                .withColumn("srch_ci_date", expedia_hotels_joined.col("srch_ci").cast("date"))
+//                .withColumn("srch_co_date", expedia_hotels_joined.col("srch_co").cast("date"))
+                .orderBy("hotel_id", "srch_ci")
+                .withColumn("lag_day", functions.lag("srch_ci", 1)
                         .over(window));
 
         Dataset<Row> df2 = df
-                .withColumn("diff", functions.datediff(df.col("srch_ci_date"), df.col("lag_day")))
+                .withColumn("diff", functions.datediff(df.col("srch_ci"), df.col("lag_day")))
                 .select("*").persist();
 
-        df2.select("id", "hotel_id","date_time", "srch_ci_date", "srch_co_date", "lag_day", "diff").show(20);
+        df2.select("id", "hotel_id","date_time", "srch_ci", "srch_co", "lag_day", "diff").show(20);
 
-        Dataset<Row> incorrect_data = df2.select("id", "hotel_id", "srch_ci_date", "srch_co_date", "lag_day", "diff")
+        Dataset<Row> incorrect_data = df2.select("id", "hotel_id", "srch_ci", "srch_co", "lag_day", "diff")
                 .where(df2.col("diff").isNotNull()
                         .and(df2.col("diff").$greater(2)
                                 .and(df2.col("diff").$less(30))));
 
         incorrect_data.show();
 
-        Dataset<Row> correct_data = df2.select("id", "hotel_id", "srch_ci_date", "srch_co_date", "lag_day", "diff")
+        Dataset<Row> correct_data = df2.select("id", "hotel_id", "srch_ci", "srch_co", "lag_day", "diff")
                 .where(df2.col("diff").isNull()
                         .or(df2.col("diff").$less(2)
                                 .or(df2.col("diff").$greater(30))));
