32f994e7e8ec39423a58200bc470f8a78c456e96
Marta_Kurman <marta.klachko@gmail.com>
2020-07-31 23:27:19 +0300

    refactoring

--- src/main/java/TaskUtil.java
+++ src/main/java/TaskUtil.java
@@ -52,6 +52,7 @@ public class TaskUtil {
 
     static Dataset<Row> valid_data(Dataset<Row> dataset) {
         return dataset.select("id", "hotel_id", "srch_ci", "srch_co", "lag_day", "diff")
+                .withColumn("value", functions.array("id", "hotel_id", "srch_ci", "srch_co", "lag_day", "diff"))
                 .where(dataset.col("diff").isNull()
                         .or(dataset.col("diff").$less(2)
                                 .or(dataset.col("diff").$greater(30))));
